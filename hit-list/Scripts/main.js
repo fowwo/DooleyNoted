8dd776d02bdea9c509287eb917aaacdf6c048ade
fowwo <54595444+fowwo@users.noreply.github.com>
2023-02-05 15:51:04 -0600

    Add ghost photos to board
    The actual images of the ghosts are not made yet.

---
+++ hit-list/Scripts/main.js
@@ -0,0 +1,53 @@
+const main = document.querySelector("main");
+
+const ghosts = [
+	"Banshee",
+	"Demon",
+	"Deogen",
+	"Goryo",
+	"Hantu",
+	"Jinn",
+	"Mare",
+	"Moroi",
+	"Myling",
+	"Obake",
+	"Oni",
+	"Onryo",
+	"Phantom",
+	"Poltergeist",
+	"Raiju",
+	"Revenant",
+	"Shade",
+	"Spirit",
+	"Thaye",
+	"The Mimic",
+	"The Twins",
+	"Wraith",
+	"Yokai",
+	"Yurei"
+];
+
+for (const ghost of ghosts) {
+	const photo = document.createElement("div");
+	photo.className = "photo";
+	photo.style.rotate = `${8 * Math.random() - 4}deg`;
+
+	const img = document.createElement("img");
+	// img.src = `Images/Ghosts/${ghost}.jpg`;
+	img.draggable = false;
+	photo.appendChild(img);
+
+	const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+	svg.setAttribute("viewBox", "0 0 80 26");
+
+	const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+	text.innerHTML = ghost;
+	text.setAttribute("x", "50%");
+	text.setAttribute("y", "50%");
+	text.setAttribute("text-anchor", "middle");
+	text.setAttribute("dominant-baseline", "central");
+
+	svg.appendChild(text);
+	photo.appendChild(svg);
+	main.appendChild(photo);
+}
