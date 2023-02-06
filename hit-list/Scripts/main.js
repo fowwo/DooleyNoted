fd8e166faad0afc6dc0275f2f41a665881825627
fowwo <54595444+fowwo@users.noreply.github.com>
2023-02-05 22:04:50 -0600

    Extract svg creation to function

--- hit-list/Scripts/main.js
+++ hit-list/Scripts/main.js
@@ -29,44 +29,36 @@ const ghosts = [
 
 for (const ghost of ghosts) {
 	const photo = document.createElement("div");
+	const div = document.createElement("div");
+	const img = document.createElement("img");
+
 	photo.className = "photo";
 	photo.style.rotate = `${8 * Math.random() - 4}deg`;
 
-	{
-		const div = document.createElement("div");
-		const img = document.createElement("img");
-		const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-		const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-		
-		// img.src = `Images/Ghosts/${ghost}.jpg`;
-		div.appendChild(img);
-		
-		svg.setAttribute("viewBox", "0 0 1 25");
-		div.appendChild(svg);
-		
-		text.setAttribute("x", "50%");
-		text.setAttribute("y", "50%");
-		text.setAttribute("text-anchor", "middle");
-		text.setAttribute("dominant-baseline", "central");
-		svg.appendChild(text);
+	// img.src = `Images/Ghosts/${ghost}.jpg`;
+	div.appendChild(img);
 
-		photo.appendChild(div);
-	}
-	{
-		const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-		const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-		
-		svg.setAttribute("viewBox", "0 0 1 22");
-		
-		text.innerHTML = ghost;
-		text.setAttribute("x", "50%");
-		text.setAttribute("y", "50%");
-		text.setAttribute("text-anchor", "middle");
-		text.setAttribute("dominant-baseline", "central");
-		svg.appendChild(text);
-		
-		photo.appendChild(svg);
-	}
+	div.appendChild(svg("", "0 0 1 25")); // Count
+
+	photo.appendChild(div);
+
+	photo.appendChild(svg(ghost, "0 0 1 22"));
 
 	main.appendChild(photo);
+}
+
+function svg(innerHTML, viewBox) {
+	const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+	const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+
+	svg.setAttribute("viewBox", viewBox);
+
+	text.innerHTML = innerHTML;
+	text.setAttribute("x", "50%");
+	text.setAttribute("y", "50%");
+	text.setAttribute("text-anchor", "middle");
+	text.setAttribute("dominant-baseline", "central");
+	svg.appendChild(text);
+
+	return svg;
 }
