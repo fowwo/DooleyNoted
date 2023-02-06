11040cf50c3c714e39fb49130ef7632262020978
fowwo <54595444+fowwo@users.noreply.github.com>
2023-02-05 19:46:57 -0600

    Add ghost count

--- hit-list/Scripts/main.js
+++ hit-list/Scripts/main.js
@@ -32,22 +32,41 @@ for (const ghost of ghosts) {
 	photo.className = "photo";
 	photo.style.rotate = `${8 * Math.random() - 4}deg`;
 
-	const img = document.createElement("img");
-	// img.src = `Images/Ghosts/${ghost}.jpg`;
-	img.draggable = false;
-	photo.appendChild(img);
+	{
+		const div = document.createElement("div");
+		const img = document.createElement("img");
+		const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+		const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+		
+		// img.src = `Images/Ghosts/${ghost}.jpg`;
+		div.appendChild(img);
+		
+		svg.setAttribute("viewBox", "0 0 1 25");
+		div.appendChild(svg);
+		
+		text.setAttribute("x", "50%");
+		text.setAttribute("y", "50%");
+		text.setAttribute("text-anchor", "middle");
+		text.setAttribute("dominant-baseline", "central");
+		svg.appendChild(text);
 
-	const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-	svg.setAttribute("viewBox", "0 0 1 22");
+		photo.appendChild(div);
+	}
+	{
+		const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+		const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+		
+		svg.setAttribute("viewBox", "0 0 1 22");
+		
+		text.innerHTML = ghost;
+		text.setAttribute("x", "50%");
+		text.setAttribute("y", "50%");
+		text.setAttribute("text-anchor", "middle");
+		text.setAttribute("dominant-baseline", "central");
+		svg.appendChild(text);
+		
+		photo.appendChild(svg);
+	}
 
-	const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-	text.innerHTML = ghost;
-	text.setAttribute("x", "50%");
-	text.setAttribute("y", "50%");
-	text.setAttribute("text-anchor", "middle");
-	text.setAttribute("dominant-baseline", "central");
-
-	svg.appendChild(text);
-	photo.appendChild(svg);
 	main.appendChild(photo);
 }
