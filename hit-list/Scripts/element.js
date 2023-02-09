35f47cd945c2b437aa5b0bddb5617caeab997fec
fowwo <54595444+fowwo@users.noreply.github.com>
2023-02-08 20:11:57 -0600

    Extract photo creation to function

--- hit-list/Scripts/element.js
+++ hit-list/Scripts/element.js
@@ -1,8 +1,10 @@
+import { toRomanNumerals } from "./func.js";
+
 /**
  * Creates automatically scaling text using an SVG element.
  */
 export function createScalableText(
-	/** @type {String} */ innerHTML, 
+	/** @type {String} */ innerHTML,
 	/** @type {String} */ viewBox
 ) {
 	const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
@@ -18,4 +20,57 @@ export function createScalableText(
 	text.setAttribute("dominant-baseline", "central");
 
 	return svg;
+}
+
+/**
+ * Creates an element containing a ghost photo.
+ */
+export function createPhoto(
+	/** @type {String} */ ghost
+) {
+	let count = 0;
+
+	const createButton = (innerText) => {
+		const button = document.createElement("button");
+		button.appendChild(createScalableText(innerText, "0 0 25 25"));
+		return button;
+	};
+
+	const photo = document.createElement("div");
+	const imageContainer = document.createElement("div");
+	const image = document.createElement("img");
+	const countElement = createScalableText("", "0 0 1 25");
+	const countTextElement = countElement.querySelector("text");
+	const buttons = document.createElement("div");
+	const minus = createButton("-");
+	const plus = createButton("+");
+
+	photo.className = "photo";
+	photo.style.rotate = `${8 * Math.random() - 4}deg`;
+	photo.appendChild(imageContainer);
+	photo.appendChild(createScalableText(ghost, "0 0 1 22"));
+
+	imageContainer.appendChild(image);
+	imageContainer.appendChild(countElement);
+	imageContainer.appendChild(buttons);
+
+	// image.src = `Images/Ghosts/${ghost}.jpg`;
+
+	buttons.appendChild(minus);
+	buttons.appendChild(plus);
+
+	minus.disabled = true;
+	minus.addEventListener("click", () => {
+		count--;
+		if (count === 0) minus.disabled = true;
+		countTextElement.innerHTML = toRomanNumerals(count);
+	});
+
+	plus.addEventListener("click", () => {
+		count++;
+		minus.disabled = false;
+		countTextElement.innerHTML = toRomanNumerals(count);
+	});
+
+	return photo;
 }
--- hit-list/Scripts/main.js
+++ hit-list/Scripts/main.js
@@ -1,79 +1,34 @@
-import { createScalableText } from "./element.js";
-import { toRomanNumerals } from "./func.js";
+import { createPhoto } from "./element.js";
 
 const main = document.querySelector("main");
 
-const ghosts = {
-	"Banshee": 0,
-	"Demon": 0,
-	"Deogen": 0,
-	"Goryo": 0,
-	"Hantu": 0,
-	"Jinn": 0,
-	"Mare": 0,
-	"Moroi": 0,
-	"Myling": 0,
-	"Obake": 0,
-	"Oni": 0,
-	"Onryo": 0,
-	"Phantom": 0,
-	"Poltergeist": 0,
-	"Raiju": 0,
-	"Revenant": 0,
-	"Shade": 0,
-	"Spirit": 0,
-	"Thaye": 0,
-	"The Mimic": 0,
-	"The Twins": 0,
-	"Wraith": 0,
-	"Yokai": 0,
-	"Yurei": 0
-};
+const ghosts = [
+	"Banshee",
+	"Demon",
+	"Deogen",
+	"Goryo",
+	"Hantu",
+	"Jinn",
+	"Mare",
+	"Moroi",
+	"Myling",
+	"Obake",
+	"Oni",
+	"Onryo",
+	"Phantom",
+	"Poltergeist",
+	"Raiju",
+	"Revenant",
+	"Shade",
+	"Spirit",
+	"Thaye",
+	"The Mimic",
+	"The Twins",
+	"Wraith",
+	"Yokai",
+	"Yurei"
+];
 
-for (const ghost in ghosts) {
-	const photo = document.createElement("div");
-	const div = document.createElement("div");
-	const img = document.createElement("img");
-	const buttons = document.createElement("div");
-	const minus = button("-");
-	const plus = button("+");
-
-	photo.className = "photo";
-	photo.style.rotate = `${8 * Math.random() - 4}deg`;
-
-	// img.src = `Images/Ghosts/${ghost}.jpg`;
-	div.appendChild(img);
-
-	const count = createScalableText("", "0 0 1 25");
-	div.appendChild(count);
-
-	photo.appendChild(div);
-
-	const text = count.querySelector("text");
-	minus.disabled = true;
-	minus.addEventListener("click", () => {
-		ghosts[ghost]--;
-		if (ghosts[ghost] === 0) minus.disabled = true;
-		text.innerHTML = toRomanNumerals(ghosts[ghost]);
-	});
-	buttons.appendChild(minus);
-
-	plus.addEventListener("click", () => {
-		ghosts[ghost]++;
-		minus.disabled = false;
-		text.innerHTML = toRomanNumerals(ghosts[ghost]);
-	});
-	buttons.appendChild(plus);
-
-	div.appendChild(buttons);
-	
-	photo.appendChild(createScalableText(ghost, "0 0 1 22"));
-
-	main.appendChild(photo);
-}
-
-function button(innerText) {
-	const button = document.createElement("button");
-	button.appendChild(createScalableText(innerText, "0 0 25 25"));
-	return button;
+for (const ghost of ghosts) {
+	main.appendChild(createPhoto(ghost));
 }
